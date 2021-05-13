dd7ac32b51f836c60f4b79db447f9e35691e79c8
Marek <marekfirlit.biz@gmail.com>
2021-05-13 11:48:13 +0100

    added navbar with bootsrap classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
  
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
